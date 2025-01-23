09083fcc926ad187053faa7a3c18b361fa2fe853
Hasan Delibaş <hasan.delibas94@gmail.com>
2025-01-23 15:03:02 +0300

    v2

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 </div>
 
 <div drawing style="position:absolute;right:0;bottom:0;width:70%;height:20%;background:#EEE" flex-y>
-  <input id="guess" type="text" style="width:100%;font-size:8.8vh;height:100%;" value="kurucu gerekli">
+  <input id="guess" type="text" style="width:100%;font-size:8.8vh;height:100%;" value="">
   <button id="guess_button" style="position: absolute;right: 0;top: 50%;transform: translate(0px, -50%);">
     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>
   </button>
